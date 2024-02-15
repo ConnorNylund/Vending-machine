75e0a94debdb02125076f2a3a13c63b69e7a512f
jlafrance3 <jlafranceiii@proton.me>
2024-02-14 20:29:56 -0500

    Added welcome message and prompt for snack selection

--- VendingMachine.java
+++ VendingMachine.java
@@ -2,4 +2,11 @@ package VendingMachine;
 
 public class VendingMachine {
     
+    public static void main(String args[]) {
+
+        //Welcome message
+        System.out.println("Welcome to Vending Machine!");
+        System.out.println("Please select from the following snacks:");
+        
+    }
 }
